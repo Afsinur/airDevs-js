b896f5f959a3170ca05d7168fa94c45c438955b8
afsinur <afsin.sayem1@gmail.com>
2023-12-30 11:05:47 +0600

    part c | adding data into an array

--- todo-app/js/app.js
+++ todo-app/js/app.js
@@ -1,11 +1,22 @@
 let todoForm = document.querySelector("[data-todo-form]");
 let todoListContainer = document.querySelector("[data-todo-list]");
 
+let dataBase = [];
+
 todoForm.addEventListener("submit", (e) => {
+  //preventing the default behavour of the from
   e.preventDefault();
-
+  //getting the input value
   let val = e.target.textData.value;
-  todoListContainer.innerHTML += `<p>${val}</p>`;
-
+  //making an object for "dataBase"
+  let obj = { todo: val };
+  //saving the data
+  dataBase.push(obj);
+  //shoing the data to the html page
+  todoListContainer.innerHTML = "";
+  dataBase.forEach((obj, i) => {
+    todoListContainer.innerHTML += `<p>${obj.todo}</p>`;
+  });
+  //reseting the form's inputs value
   e.target.reset();
 });
