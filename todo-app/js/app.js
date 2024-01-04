2324045344ca3c7a1b371e23cf8221ec16626f54
afsinur <afsin.sayem1@gmail.com>
2024-01-04 11:33:58 +0600

    part e | After removing showing data to page

--- todo-app/js/app.js
+++ todo-app/js/app.js
@@ -3,60 +3,54 @@ let todoListContainer = document.querySelector("[data-todo-list]");
 
 let dataBase = [];
 
+function deleteAndReturnAnArray(e) {
+  let filteredArray = dataBase.filter((obj, i) => {
+    if (obj.id != e) {
+      return true;
+    }
+  });
+
+  return filteredArray;
+}
+
+function runAfterAddClick() {
+  let allTodoP = document.querySelectorAll("[data-todo-id]");
+
+  allTodoP.forEach((itm) => {
+    itm.addEventListener("dblclick", (e) => {
+      //after delete update database
+      dataBase = deleteAndReturnAnArray(e.target.dataset.todoId);
+      showDbToHtmlPage();
+    });
+  });
+}
+
 todoForm.addEventListener("submit", (e) => {
   //preventing the default behavour of the from
   e.preventDefault();
   //getting the input value
   let val = e.target.textData.value;
   //making an object for "dataBase"
-  let obj = { id: mkId(), todo: val };
+  let obj = { id: `id_${Date.now()}`, todo: val };
   //saving the data
   dataBase.push(obj);
-  //shoing the data to the html page
-  todoListContainer.innerHTML = "";
-  dataBase.forEach((obj, i) => {
-    todoListContainer.innerHTML += `
-      <p data-todo-id="${obj.id}">${obj.todo}</p>
-    `;
-  });
-  console.log(dataBase);
-
-  let allTodoP = document.querySelectorAll("[data-todo-id]");
-  allTodoP.forEach((itm) => {
-    itm.addEventListener("click", (e) => {
-      console.log(e.target.dataset.todoId);
-    });
-  });
+  showDbToHtmlPage();
   //reseting the form's inputs value
   e.target.reset();
 });
 
-function mkId() {
-  let randomNumber = `${Math.random()}`;
-  let onlyNumbers = randomNumber.slice(2, randomNumber.length);
-  let uniqID = `id_${onlyNumbers}`;
+function showDbToHtmlPage() {
+  //shoing the data to the html page
+  todoListContainer.innerHTML = "";
+  dataBase.forEach((obj, i) => {
+    todoListContainer.innerHTML += `
+      <p data-todo-id="${obj.id}" class="p-2 bg-gray-100 cursor-pointer text-sm font-semibold">
+        <span class="select-none pointer-events-none">
+          ${obj.todo}
+        </span>
+      </p>
+      `;
+  });
 
-  return uniqID;
+  runAfterAddClick();
 }
-
-// const arr = [
-//   { id: "id_5638316917048742", todo: "1. ghum theke utha" },
-//   { id: "id_779154663243744", todo: "2. namaz pora" },
-//   { id: "id_26911164250716624", todo: "3. hatte jaoya" },
-//   { id: "id_10896248764510719", todo: "4. ca khaoya" },
-//   { id: "id_9854961201947519", todo: "5. kaje bosa" },
-// ];
-
-// function deleteAndReturnAnArray(e) {
-//   let filteredArray = arr.filter((obj, i) => {
-//     if (obj.id != e) {
-//       return true;
-//     }
-//   });
-
-//   return filteredArray;
-// }
-
-// let filteredArr = deleteAndReturnAnArray("id_779154663243744");
-
-// console.log(filteredArr);
